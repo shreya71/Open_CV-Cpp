852916fce7d17e9cb6ef153be019524876bc9645
Shreya <91543981+shreya71@users.noreply.github.com>
2022-06-08 20:53:18 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,16 +6,13 @@
 using namespace cv;
 using namespace std;
 
-
-/////////////////  Images  //////////////////////
-
-int main() {
-    // Mat img = imread("1.jpg");
-     //namedWindow("image", WINDOW_NORMAL);
-    // imshow("image", img);    
-    string path = "1.jpg";/////
-    Mat img = imread(path);/////
-    imshow("Image", img);////
-    waitKey(0);
-    return 0;
+void main() {   
+    VideoCapture cap(0);/////
+    Mat img ;
+    while (true)
+    {
+        cap.read(img);/////
+        imshow("Image", img);////
+        waitKey(1);
+    }
 }
