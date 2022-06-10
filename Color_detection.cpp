b9cac9194b297aa46ece7c009ef5d82b35bd529f
Shreya <91543981+shreya71@users.noreply.github.com>
2022-06-10 17:03:42 +0530

    Create Color_detection.cpp

---
+++ Color_detection.cpp
@@ -0,0 +1,37 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void main() {
+
+	string path = "book.png";
+	Mat img = imread(path);
+	Mat imgHSV, simplified;
+	int h_min = 0, s_min = 0, v_min = 0, h_max = 199, s_max = 255, v_max = 255;
+
+	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+	namedWindow("Tracker");
+	createTrackbar("Hue Min", "Trackbars", &h_min, 179);
+	createTrackbar("Hue Max", "Trackbars", &h_max, 179);
+	createTrackbar("Sat Min", "Trackbars", &s_min, 255);
+	createTrackbar("Sat Max", "Trackbars", &s_max, 255);
+	createTrackbar("Val Min", "Trackbars", &v_min, 255);
+	createTrackbar("Val Max", "Trackbars", &v_max, 255);
+
+	while(true) {
+
+		Scalar lower(h_min, s_min, v_min);
+		Scalar upper(h_max, s_max, v_max);
+		inRange(imgHSV, lower, upper, simplified);
+
+		imshow("Image", img);
+		imshow("Image HSV", imgHSV);
+		imshow("Image Mask", simplified);
+		waitKey(1);
+	}
+}
